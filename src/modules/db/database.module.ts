f312bb05f496b32a568cdfd8a7a5206621bd7d5e
Tran Duy Nghia <duy.nghia.tran1@ibm.com>
2022-04-19 16:54:41 +0200

    Fix DB config

--- src/modules/db/database.module.ts
+++ src/modules/db/database.module.ts
@@ -11,22 +11,33 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        url: configService.get<string>('DATABASE_URL'),
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: parseInt(configService.get<string>('DATABASE_PORT')),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        migrations: ['dist/src/migration/**/*{.ts,.js}'],
-        subscribers: ['dist/src/subscriber/**/*{.ts,.js}'],
-        synchronize: configService.get<string>('NODE_ENV') === 'development',
-        extra: {
-          ssl: process.env.DATABASE_URL ? true : false,
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        let config: TypeOrmModuleOptions = {
+          url: configService.get<string>('DATABASE_URL'),
+          type: 'postgres',
+          host: configService.get<string>('DATABASE_HOST'),
+          port: parseInt(configService.get<string>('DATABASE_PORT')),
+          username: configService.get<string>('DATABASE_USERNAME'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          migrations: ['dist/src/migration/**/*{.ts,.js}'],
+          subscribers: ['dist/src/subscriber/**/*{.ts,.js}'],
+          synchronize: configService.get<string>('NODE_ENV') === 'development',
+        };
+
+        if (configService.get<string>('NODE_ENV') === 'production') {
+          config = {
+            ...config,
+            extra: {
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            },
+          };
+        }
+        return config;
+      },
     }),
   ],
 })
